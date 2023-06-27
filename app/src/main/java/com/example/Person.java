2af8f3186ae2bf184c27a1f75128e2a479835784
Ira <37289156+dickenz@users.noreply.github.com>
2023-06-27 11:31:24 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/Person.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Person {
+
+    private final String name;
+    private final String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
